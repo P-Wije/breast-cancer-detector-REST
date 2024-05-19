b66a99ae27afe0ab236ce28b343f39a28a8ff262
irusha <irusha@codegen.net>
2024-05-19 20:43:35 +0530

    Revert "Added mongo support"
    This reverts commit af31a7569ca10c37ebf9e45461015bebfde4590a.

--- cancerDetectionREST/settings.py
+++ cancerDetectionREST/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import mongoengine
+HOST = "127.0.0.1"
+USERNAME = "admin"
+PASSWORD = "admin"
+mongoengine.connect(db="breast-cancer-detection", host=f"mongodb+srv://{HOST}/",
+                    username=USERNAME, password=PASSWORD)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +82,8 @@ WSGI_APPLICATION = 'cancerDetectionREST.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'breast-cancer-detection',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
